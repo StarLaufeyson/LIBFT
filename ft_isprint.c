d366f569534e5e576232ecd836f901bfa8134aaf
Estrella Luno Lago <eluno-la@c3r2s3.42urduliz.com>
2023-05-10 18:22:09 +0200

    isprint

--- ft_isprint.c
+++ ft_isprint.c
@@ -6,12 +6,28 @@
 /*   By: eluno-la <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/29 17:25:20 by eluno-la          #+#    #+#             */
-/*   Updated: 2023/04/29 17:36:26 by eluno-la         ###   ########.fr       */
+/*   Updated: 2023/05/10 18:21:53 by eluno-la         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	<ctype.h>
 #include	<stdio.h>
+#include	<unistd.h>
 /*#include	<libft.h>*/
 
-int	ft_isprint(
+int	ft_isprint(int c)
+{
+	if (c >= 32 && c <= 126)
+	{
+		return (1);
+	}
+	return (0);
+}
+
+int	main(void)
+{
+	printf("%d\n", ft_isprint('A'));
+	printf("%d\n", ft_isprint('~'));
+	printf("%d\n", ft_isprint('$'));
+	return (0);
+}
