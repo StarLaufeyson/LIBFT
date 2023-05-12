98d7240402e1252b3c629bd43d5a20e591b2505c
Estrella Luno Lago <eluno-la@c3r6s1.42urduliz.com>
2023-05-12 13:46:03 +0200

    ft_strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -3,28 +3,45 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eluno-la <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: eluno-la <eluno-la@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/29 17:42:32 by eluno-la          #+#    #+#             */
-/*   Updated: 2023/04/29 18:29:42 by eluno-la         ###   ########.fr       */
+/*   Updated: 2023/05/12 13:41:45 by eluno-la         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include	<unistd.h>
+#include	<stddef.h>
 #include	<stdio.h>
 /*#include	<libft.h>*/
 
-int	ft_strlen(char *str)
+size_t	ft_strlcpy(char *dest, const char *src, size_t dest_size)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
-	while (str[i] != '\0')
+	while (*src && (i + 1 < dest_size))
+	{
+		*dest++ = *src++;
 		i++;
+	}
+	if (dest_size > 0)
+	{
+		*dest = '\0';
+	}
+	while (*src++)
+	i++;
 	return (i);
 }
 
-unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+int	main(void)
 {
+	char	src[] = "Hello, World!";
+	char	dest[20];
 
+	size_t length = ft_strlcpy(dest, src, sizeof(dest));
+
+	printf("Cadena de origen: %s\n", src);
+	printf("Cadena de destino: %s\n", dest);
+	printf("Longitud de la cadena copiada: %zu\n", length);
+	return (0);
 }
