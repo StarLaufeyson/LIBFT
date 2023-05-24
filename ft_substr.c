eca9f24ee420113835665322cd5abd8a8df4352d
Estrella Luno Lago <eluno-la@c3r2s1.42urduliz.com>
2023-05-24 18:43:24 +0200

    substr

---
+++ ft_substr.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eluno-la <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/24 17:47:06 by eluno-la          #+#    #+#             */
+/*   Updated: 2023/05/24 18:41:26 by eluno-la         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include	"libft.h"
+
+void	*ft_memcpy(void *dest, const coid *src, size_t length)
+{
+	char	*d;
+	const char	*s;
+	size_t	i;
+
+	d = dest;
+	s = src;
+	i = 0;
+	while (i < length)
+	{
+		d[i] = s[i];
+		i++;
+	}
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	len;
+
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+	return (len);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	s_len;
+	size_t	sub_len;
+	char	*substr;
+
+	s_len = ft_strlen(s);
+	sub_len = len;
+	//Verificar si el índice de inicio esta fuera de los límites
+	if (start >= s_len)
+		return (NULL);
+	//calcular longitud de la subcadena
+	if (sub_len > (s_len - start))
+	//Garantizamos que la subcadena no exceda los límites de la cadena original
+		sub_len = s_len - start;
+	//Asignar memoria a la subcadena
+	substr = (char *)malloc((sub_len + 1) * sizeof(char));
+	//sub_len + 1 reserva espacio para la subcadena, + 1 (para añadir el '\0')
+	if (substr == NULL)
+		return (NULL);
+	//Copiar los caracteres de la cadena a la subcadena
+	ft_memcpy(substr, s + start, sub_len);
+		substr[sub_len] = '\0';
+		return (substr);
+}
+
+int	main(void)
+{
+	const char	*s = "Hello World";
+	unsigned int	start = 7;
+	size_t	len = 5;
+
+	char	*substr = ft_substr(s, start, len);
+	if (substr != NULL)
+	{
+		printf("Subcadena: %s\n", substr);
+		free (substr); //Liberar memoria aasignada
+	}
+	else
+	{
+		printf("Error al extraer la subcadena.\n");
+	}
+	return (0);
+}
