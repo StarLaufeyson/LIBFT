b966a1f0e6672afa1abe01708a88094ab5df908f
StarLaufeyson <130982199+StarLaufeyson@users.noreply.github.com>
2023-06-16 21:43:29 +0200

    Update ft_putnbr_fd.c

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: eluno-la <eluno-la@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 18:01:25 by eluno-la          #+#    #+#             */
-/*   Updated: 2023/06/02 18:58:42 by eluno-la         ###   ########.fr       */
+/*   Updated: 2023/06/16 18:23:34 by eluno-la         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,28 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	c;
-
+	if (n == -2147483648)
+	{
+		ft_putstr_fd("-2147483648", fd);
+		return ;
+	}
+	if (n == 0)
+	{
+		ft_putchar_fd('0', fd);
+		return ;
+	}
 	if (n < 0)
 	{
-		write(fd, "-", 1);
-		n = -n;
+		ft_putchar_fd('-', fd);
+		n *= -1;
 	}
 	if (n >= 10)
+	{
 		ft_putnbr_fd(n / 10, fd);
-	c = n % 10 + '0';
-	write(fd, &c, 1);
+		ft_putnbr_fd(n % 10, fd);
+	}
+	else
+		ft_putchar_fd(n + '0', fd);
 }
 
 /*int	main(void)
